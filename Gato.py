330f3b9a56e878cdcd6f56ce7062a3764bc6f8de
JesusMR09 <113972032+JesusMR09@users.noreply.github.com>
2024-05-21 20:54:27 +0200

    Add files via upload

---
+++ Gato.py
@@ -0,0 +1,16 @@
+"""
+Clase Gato.
+:author: Jaime Rabasco Ronda.
+<<<<<<< HEAD
+
+=======
+belen leon
+>>>>>>> refactorizacion
+"""
+class Gato:
+
+    def maullar(self):
+        print('Miau');
+
+g = Gato();
+g.maullar();
